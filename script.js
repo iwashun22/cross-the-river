81e7cfbfa2ebe9453828c5dd864585181dd1b310
Shun Iwashita <iwashunabc519@gmail.com>
2021-10-02 10:48:38 +0700

    add rivers and scroll the screen

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 // set game board
-const pixelSize = 15;
+const pixelSize = 20;
 const boardLength = 21;
 const boardHeight = 25;
 
@@ -18,24 +18,35 @@ console.log(ctx);
 
 const game = {
    player: {
-      size: 14,
+      size: 16,
       positionX: 0,
       positionY: 0,
       directionX: 0,
       directionY: 0,
-      cooldown: 0,
+      countCooldown: 0,
+      cooldown: 24
    },
    isOver: true,
    moving: null,
-   rivers: []
+   rivers: [],
+   maxRiversRows: 5,
+   countRivers: 0,
+   spaceBetweenRivers: 0
 };
 
 class river{
    constructor(y){
-      this.log = [];
+      this.logs = [];
       this.positionY = y;
       this.length = pixelSize;
-      this.direction = (Math.floor(Math.random() * 6) + 1) * 3;
+      this.countCooldown = 0;
+      this.cooldown = (Math.floor(Math.random() * 6) + 1) * 3;
+
+      const randomDirection = Math.floor(Math.random() * 2);
+      if(randomDirection == 0)
+         this.direction = 'left';
+      else 
+         this.direction = 'right';
    }
 }
 
@@ -49,20 +60,38 @@ function init(){
       // @ts-ignore
       canvas.height / 2
    )
+   game.isOver = true;
+   game.moving = null;
+   game.rivers = [];
 }
 init();
 
 function startGame(){
    game.isOver = false;
    game.player.direction = 0;
-   game.player.cooldown = 0;
+   game.player.countCooldown = 0;
 
    // @ts-ignore
    game.player.positionX = canvas.width / 2;
    // @ts-ignore
-   game.player.positionY = canvas.height - (game.player.size / 2);
+   game.player.positionY = canvas.height - (pixelSize / 2);
+
+   createDefaultRivers();
    game.moving = setInterval(ticker, 10);
 }
+
+function createDefaultRivers(){
+   for(let i = 0; i < boardHeight; i+= Math.floor(Math.random() * 4) + 1){
+      let randomY = canvas.height - (pixelSize / 2) - (pixelSize * i);
+      
+      /// we don't want to have rivers in first four line
+      if(randomY < canvas.height - (pixelSize * 4)){
+         let r = new river(randomY);
+         game.rivers.push(r);
+      }
+   }
+}
+
 
 function ticker(){
    scrollScreen();
@@ -71,8 +100,6 @@ function ticker(){
    //moveRiver();
    movePlayer();
 
-   // @ts-ignore
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
    drawBackground();
    //drawLog();
    drawPlayer();
@@ -84,9 +111,25 @@ function scrollScreen(){
    ){
       game.player.positionY += pixelSize;
 
-      game.rivers = game.rivers.map(r => 
-         r.positionY + pixelSize
-      )
+      game.rivers.forEach(r => {
+         r.positionY += pixelSize;
+      })
+
+      game.rivers = game.rivers.filter(r => r.positionY < canvas.height);
+
+      if(game.countRivers <= 0 && game.spaceBetweenRivers <= 0){
+         game.countRivers = Math.floor(Math.random() * game.maxRiversRows) + 1;
+         game.spaceBetweenRivers = Math.floor(Math.random() * 4) + 1;
+      }
+      
+      
+      if(game.countRivers > 0 && game.spaceBetweenRivers > 0){
+         let r = new river(pixelSize / 2);
+         game.rivers.push(r);
+
+         game.countRivers--;
+      }
+      else game.spaceBetweenRivers--;
    }
 }
 
@@ -98,20 +141,33 @@ function movePlayer(){
    game.player.directionX = 0;
    game.player.directionY = 0;
    
-   game.player.cooldown <= 0?
-      game.player.cooldown = 0
+   game.player.countCooldown <= 0?
+      game.player.countCooldown = 0
    :
-      game.player.cooldown--;
+      game.player.countCooldown--;
 }
 
 function drawBackground(){
+   // @ts-ignore
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
    ctx.fillStyle = '#FFF8C2';
    // @ts-ignore
    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+   game.rivers.forEach(r => {
+      ctx.fillStyle = '#00C5EB';
+      ctx.fillRect(
+         0, 
+         r.positionY - (pixelSize / 2),
+         canvas.width,
+         pixelSize
+      )
+   })
 }
 
 function drawPlayer(){
-   ctx.fillStyle = '#00DC14';
+   ctx.fillStyle = '#0A9F00';
    const halfSize = game.player.size / 2;
    ctx.fillRect(
       game.player.positionX - halfSize,
@@ -126,7 +182,7 @@ document.addEventListener('keydown', (e) => {
       startGame();
    }
 
-   if(game.player.cooldown == 0 && !game.isOver){
+   if(game.player.countCooldown == 0 && !game.isOver){
       if(e.key == 'ArrowUp'){
          game.player.directionY = -pixelSize;
       }
@@ -136,16 +192,16 @@ document.addEventListener('keydown', (e) => {
          game.player.directionY = pixelSize;
       }
       else if(e.key == 'ArrowLeft' &&
-               game.player.positionX > pixelSize
+               game.player.positionX >= (pixelSize * 3 / 2)
       ){
          game.player.directionX = -pixelSize;
       }
       else if(e.key == 'ArrowRight' &&
-               game.player.positionX < canvas.width - pixelSize
+               game.player.positionX <= canvas.width - (pixelSize * 3 / 2)
       ){
          game.player.directionX = pixelSize;;
       }
 
-      game.player.cooldown = 24;
+      game.player.countCooldown = game.player.cooldown;
    }
 })
